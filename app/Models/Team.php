c57dae71ef8726b160d37a36905de6a9a628a6cd
Jack Coughlan <jcoughlan@softworks.com>
2024-11-06 12:13:47 +0000

    Added missing comment and some db images

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//Represents Teams table in database
 class Team extends Model
 {
     use HasFactory;
